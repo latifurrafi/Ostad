f705e8211b56a0f0ade06ce4aea2817b63f775c0
Rafi <latifurrafi@gmail.com>
2024-11-24 23:17:34 +0600

    OOP

---
+++ oop.py
@@ -0,0 +1,16 @@
+class Myclass:
+    x = 20
+    y = 30
+    z = 40
+
+    sum = x+y+z
+
+obj1 = Myclass()
+print(obj1.x)
+
+obj2 = Myclass()
+print(obj2.y)
+
+obj3 = Myclass()
+print(obj3.z)
+print(obj3.sum())
